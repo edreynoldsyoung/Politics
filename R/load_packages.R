b650cc1e51c0aea4d2523272d2eb162c455e84af
Ed Reynolds <edreynoldsyoung@gmail.com>
2025-03-26 19:53:25 -0400

    First Draft

---
+++ R/load_packages.R
@@ -0,0 +1,61 @@
+check_load_pkgs <- function() {
+  if (!require(tidyverse)) {
+    message("installing the 'tidyverse' package")
+    install.packages(tidyverse)
+  }
+  if (!require(ggrepel)) {
+    message("installing the 'ggrepel' package")
+    install.packages(ggrepel)
+  }
+  if (!require(lubridate)) {
+    message("installing the 'lubridate' package")
+    install.packages(lubridate)
+  }
+  if (!require(EpiEstim)) {
+    message("installing the 'EpiEstim' package")
+    install.packages(EpiEstim)
+  }
+  if (!require(RSocrata)) {
+    message("installing the 'RSocrata' package")
+    install.packages(RSocrata)
+  }
+  if (!require(covid19.analytics)) {
+    message("installing the 'covid19.analytics' package")
+    install.packages(covid19.analytics)
+  } 
+  if (!require(knitr)) {
+    message("installing the 'knitr' package")
+    install.packages(knitr)
+  }
+  if (!require(sf)) {
+    message("installing the 'sf' package")
+    install.packages(sf)
+  }
+  if (!require(tigris)) {
+    message("installing the 'tigris' package")
+    install.packages(tigris)
+  }
+  if (!require(usmap)) {
+    message("installing the 'usmap' package")
+    install.packages(usmap)
+  }
+  if (!require(tidycensus)) {
+    message("installing the 'tidycensus' package")
+    install.packages(tidycensus)
+  }
+  if (!require(scales)) {
+    message("installing the 'scales' package")
+    install.packages(scales)
+  }
+  if (!require(viridis)) {
+    message("installing the 'viridis' package")
+    install.packages(viridis)
+  }
+  if (!require(ggridges)) {
+    message("installing the 'ggridges' package")
+    install.packages(ggridges)
+  }  
+}
+check_load_pkgs()
+options(tigris_class = "sf")
+options(tigris_use_cache = TRUE)
